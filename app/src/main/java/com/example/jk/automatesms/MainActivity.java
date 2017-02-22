7f5a91833a833444974b9939dbd25ab40792216d
jagigokul <jagigokulgit@gmail.com>
2017-02-22 11:27:47 +0530

    Contact Picker Code added

--- app/src/main/java/com/example/jk/automatesms/MainActivity.java
+++ app/src/main/java/com/example/jk/automatesms/MainActivity.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -21,6 +27,13 @@ public class MainActivity extends Activity {
 
     private int hour, minute;
 
+    //ContactPicker
+    private static final int RESULT_PICK_CONTACT = 85500;
+    private TextView contact_name;
+    private TextView contact_mobile;
+    private Button pickContact;
+
+
     EditText date;
     EditText time;
 
@@ -30,6 +43,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         date = (EditText)findViewById(R.id.date);
         time = (EditText)findViewById(R.id.time);
+
+        //contact picker
+        contact_name = (TextView) findViewById(R.id.contact_name);
+        contact_mobile = (TextView) findViewById(R.id.contact_mobile);
+        pickContact = (Button)findViewById(R.id.pickContact);
+
 
         calendar = Calendar.getInstance();
 
@@ -106,4 +125,59 @@ public class MainActivity extends Activity {
         date.setText(new StringBuilder().append(day).append("/")
                 .append(month).append("/").append(year));
     }
+
+
+
+    //ContactPickerCode
+
+
+    public void pickContact(View v)
+    {
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, RESULT_PICK_CONTACT);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // check whether the result is ok
+        if (resultCode == RESULT_OK) {
+            // Check for the request code, we might be usign multiple startActivityForReslut
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Log.e("MainActivity", "Failed to pick contact");
+        }
+    }
+    /**
+     * Query the Uri and read contact details. Handle the picked contact data.
+     * @param data
+     */
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+        try {
+            String phoneNo = null ;
+            String name = null;
+            // getData() method will have the Content Uri of the selected contact
+            Uri uri = data.getData();
+            //Query the content uri
+            cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+            // column index of the phone number
+            int  phoneIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            // column index of the contact name
+            int  nameIndex =cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            phoneNo = cursor.getString(phoneIndex);
+            name = cursor.getString(nameIndex);
+            // Set the value to the textviews
+            contact_name.setText(name);
+            contact_mobile.setText(phoneNo);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
